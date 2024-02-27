3441753e6ea489fb94f8f4c02848b881b814fe9a
esrAA312 <esraayahya312@gmail.com>
2024-02-27 09:47:31 -0800

    done

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -2,32 +2,30 @@
 
 /**
  * heap_insert - inserts a value in a Binary Search Tree
- * @tree: double pointer to the root node of the HEAP to insert the value
+ * @root double pointer to the root node of the HEAP to insert the value
  * @value: value to store in the node to be inserted
  * Return: a pointer to the created node or NULL
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *new;
-	heap_t **tree;
 
-	tree=root;
-	if (!tree)
+	if (!root)
 		return (NULL);
 
-	if (!*tree)
+	if (!*root)
 	{
 		new = binary_tree_node(NULL, value);
-		return (new ? *tree = new : NULL);
+		return (new ? *root = new : NULL);
 	}
 
-	return ((*tree)->n > value
-			? ((*tree)->left
-				? heap_insert(&((*tree)->left), value)
-				: ((*tree)->left = binary_tree_node(*tree, value)))
-			: ((*tree)->n < value
-				? ((*tree)->right
-					? heap_insert(&((*tree)->right), value)
-					: ((*tree)->right = binary_tree_node(*tree, value)))
+	return ((*root)->n > value
+			? ((*root)->left
+				? heap_insert(&((*root)->left), value)
+				: ((*root)->left = binary_tree_node(*root, value)))
+			: ((*root)->n < value
+				? ((*root)->right
+					? heap_insert(&((*root)->right), value)
+					: ((*root)->right = binary_tree_node(*root, value)))
 				: NULL));
 }
